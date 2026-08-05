95fbf60c9e21f73db892f5ea9b2782bf828cddcd
tenadvocate-beep <tenadvocate@gmail.com>
2026-08-05 17:40:50 +0400

    Fix duplicate transporter

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,16 +1,6 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    family: 4,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-});
-
-
-const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
