b9779e854bfb3320c00931eb06f13674968c8461
tenadvocate-beep <tenadvocate@gmail.com>
2026-08-05 17:09:13 +0400

    Add SMTP connection check

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -8,6 +8,15 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("SMTP Verify Error:", error);
+    } else {
+        console.log("SMTP server is ready.");
+    }
+});
+
 const sendEmail = async (options) => {
 
     try {
